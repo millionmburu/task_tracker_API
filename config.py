7de96779dc381f8268fb6ef3221a408e66b1f86c
Million <million.mburu@student.moringaschool.com>
2026-09-09 21:19:15 +0300

    Created User and Table models, initial migration, password hashing

---
+++ config.py
@@ -0,0 +1,30 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+#flask instance
+app = Flask(__name__)
+
+#~~~~~~~~ Database config ~~~~~~~~#
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#~~~~~~~~ JWT config ~~~~~~~~# # TODO
+
+# extensions initializations
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+api = Api(app)
+jwt = JWTManager(app)
+CORS(app)
+
+
+
+
+
